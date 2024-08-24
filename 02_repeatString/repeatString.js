71b09b06d89d7fd79954aaa1acca65947f537df6
pepse3000 <113620477+pepse3000@users.noreply.github.com>
2024-08-24 20:23:56 +0300

    complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, number) {
+    if (number < 0) {
+        return 'ERROR'
+    }
 
+    fullString = ""
+
+    for (let i = 0; i < number; i++) {
+        fullString += string
+    }
+
+    return fullString
 };
 
 // Do not edit below this line
