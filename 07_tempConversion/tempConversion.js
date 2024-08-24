7b39e205c08d9570da0b6d881145e815bb91f46d
pepse3000 <113620477+pepse3000@users.noreply.github.com>
2024-08-24 21:34:40 +0300

    complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  result = (temp - 32) / 1.8;
+  
+  if (Number.isInteger(result)) {
+    return result;
+  } else {
+    return parseFloat(result.toFixed(1))
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  result = temp * 1.8 + 32
+
+  if (Number.isInteger(result)) {
+    return result;
+  } else {
+    return parseFloat(result.toFixed(1))
+  }
 };
 
 // Do not edit below this line
