d12f249e3c20b37043c0ca2a29f407dd530596e4
pepse3000 <113620477+pepse3000@users.noreply.github.com>
2024-08-24 21:22:55 +0300

    complete

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,14 @@
-const leapYears = function() {
+const leapYears = function(number) {
+    if (number % 4 == 0 && number % 100 != 0) {
+        return true;
+    } else if (number % 400 == 0) {
+        return true;
+    } else {
+        return false;
+    }
 
 };
+
 
 // Do not edit below this line
 module.exports = leapYears;
