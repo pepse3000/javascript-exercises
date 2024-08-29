85eccedbb28bfd571b164d145c1be8d073a208d9
pepse3000 <113620477+pepse3000@users.noreply.github.com>
2024-08-29 18:29:01 +0300

    complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,32 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, current) => sum + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((sum, current) => sum * current)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0) return 1;
+  let result = 1;
+  for (let i = 1; i <= num; i ++) {
+    result *= i;
+  }
+  return result;
 };
+
 
 // Do not edit below this line
 module.exports = {
