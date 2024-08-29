15a68c91252ae2b8220e41600ed52730c5086b41
pepse3000 <113620477+pepse3000@users.noreply.github.com>
2024-08-29 19:19:35 +0300

    complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num < 0) {
+        return "OOPS"
+    }
 
+    let fibonacciArr = [0, 1, 1, 2, 3];
+
+    for (let i = 0; i < parseInt(num); i ++) {
+        let result = fibonacciArr[fibonacciArr.length - 1] + fibonacciArr[fibonacciArr.length - 2];
+        let newArr = [fibonacciArr[fibonacciArr.length - 1], result];
+        fibonacciArr.push(newArr[1]);
+    }
+
+    return fibonacciArr[parseInt(num)];
 };
 
+
+console.log(fibonacci("3"));
 // Do not edit below this line
 module.exports = fibonacci;
