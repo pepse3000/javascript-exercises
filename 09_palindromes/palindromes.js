5a09aa8527dd14cec6e1d2cf6272a4b3d633d36d
pepse3000 <113620477+pepse3000@users.noreply.github.com>
2024-08-29 19:02:23 +0300

    complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const symbolsToRemove = [" ", ".", ",", "!"];
+    let editedStr = str
+        .toLowerCase()
+        .split("")
+        .filter(elem => !symbolsToRemove.includes(elem));
 
+    return editedStr.join("") == editedStr.reverse().join("");
 };
 
+console.log(palindromes("A car, a man, a maraca."));
+
 // Do not edit below this line
+
 module.exports = palindromes;
