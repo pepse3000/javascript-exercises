2a9ff1bbc56757dd8fbd20e69c3dc59f14917b6b
pepse3000 <113620477+pepse3000@users.noreply.github.com>
2024-08-29 19:41:32 +0300

    complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    const TODAYS_YEAR = 2024;
 
+    arr.map((person) => {
+        if (!person.yearOfDeath) {
+            person.age = TODAYS_YEAR - person.yearOfBirth;
+        } else {
+            person.age = person.yearOfDeath - person.yearOfBirth;
+        }
+    })
+    
+    arr.sort((a, b) => b.age - a.age);
+    
+
+    return arr[0];
 };
 
 // Do not edit below this line
